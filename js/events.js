ff18a160ed6cf65f65d712554f0707b543582389
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2024-11-30 16:11:27 -0600

    add limit for events until we add extended scrolling

--- js/events.js
+++ js/events.js
@@ -1,8 +1,8 @@
-import { getDoc, doc, getDocs, query, setDoc, collection, orderBy, addDoc, Timestamp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
+import { getDoc, doc, getDocs, query, setDoc, collection, orderBy, addDoc, Timestamp, limit } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
 import { displayEvent } from "./global.js";
 import { db } from "./firebase.js";
 
-const events = await getDocs(query(collection(db, "posts"), orderBy("timestamp", "desc")))
+const events = await getDocs(query(collection(db, "posts"), orderBy("timestamp", "desc"), limit(100)))
 
 events.forEach(async ev => {
     const data = ev.data()
