b92cf933a3021946fd60623d669fa10f1753e857
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2024-12-01 16:33:27 -0600

    Simplify displayEvent, add variety of methods for working with user data and event data

--- js/funcs.js
+++ js/funcs.js
@@ -92,163 +92,152 @@ export function addItem(label, img, href, id, parent, expanded, expandedContent,
 
 }
 
-export async function displayEvent(eventId, content=document.getElementById("content")) {
-    // get data from eid
+export async function displayEvent(id, content=document.getElementById("content")) {
     
-    let e = await getDoc(doc(db, "posts", eventId))
+    const event = await getEvent(id)
 
-    if (e.exists()) {
-        let eventData = e.data()
-        let u = await getDoc(doc(db, "users", eventData.creator, "data", "public"))
-        if (u.exists()) {
-            let userData = u.data()
-            let username = await getDoc(doc(db, "usernames", eventData.creator))
+    const user = await getUserData(event.creator)
 
-            if (username.exists()) {
-                const usrname = username.data().username
+    const username = await getUsername(event.creator)
 
-                await display(eventId, eventData, userData, usrname)
+    let cost = "Free admission";
 
-            }
-
-            
-
-        };
+    if (event.cost > 0) {
+        cost = `$${event.cost} per person`
     }
 
-    async function display(id, event, user, username) {
+    // make event
+    const ev = document.createElement("div")
+    ev.classList.add("event")
+    ev.id = id
 
-        let cost = "Free admission";
-
-        let attending = 0
-        
-        let selfAttend = false
-
-        const uData = await getDocs(query(collection(db, "posts", id, "uData")))
-
-        uData.forEach((doc) => {
-            const data = doc.data()
-
-            if (data.attending) {
-                attending += 1
-            }
-            if (doc.id == auth.currentUser.uid && data.attending) selfAttend = true;
-        })
-
-        if (event.cost > 0) {
-            cost = `$${event.cost} per person`
-        }
-        // make event
-        const ev = document.createElement("div")
-        ev.classList.add("event")
-        ev.id = id
-
-        ev.innerHTML = `
-        <img class="pfp border" src="../img/pfp.jpg">
-        <div class="event-content">
-            <div class="user-info row" style="gap: 5px;">
-                <h4 class="display-name">${user.displayName}</h4>
-                <div class="row" style="gap: 5px;">
-                    <h4 class="username">@${username}</h4>
-                    <span class="bullet">•</span>
-                    <h4 class="category">${event.category}</h4>
-                </div>
-
+    ev.innerHTML = `
+    <img class="pfp border" src="../img/pfp.jpg">
+    <div class="event-content">
+        <div class="user-info row" style="gap: 5px;">
+            <h4 class="display-name">${user.displayName}</h4>
+            <div class="row" style="gap: 5px;">
+                <h4 class="username">@${username}</h4>
+                <span class="bullet">•</span>
+                <h4 class="category">${event.category}</h4>
             </div>
-            <div class="event-details row">
-                <span><b>${event.date}</b></span>
-                |
-                <span><b>${event.location}</b></span>
-                |
-                <span><b>${cost}</b></span>
-            </div>
-            <p>
-                ${event.desc}
-            </p>
-            <!-- <img class="event-image" src="../img/sample.jpg"> -->
-        
-            <div class="actions">
-                
-            </div>
-                
+
+        </div>
+        <div class="event-details row">
+            <span><b>${event.date}</b></span>
+            |
+            <span><b>${event.location}</b></span>
+            |
+            <span><b>${cost}</b></span>
+        </div>
+        <p>
+            ${event.desc}
+        </p>
+        <!-- <img class="event-image" src="../img/sample.jpg"> -->
+    
+        <div class="actions">
             
         </div>
-        `
-        content.appendChild(ev)
-
-        const left = document.createElement("div")
-        left.classList.add("row")
-
-        const actions = ev.querySelector(`.actions`)
-
-        actions.append(left)
-
-        function addAction(l, src, func) {
-            const action = document.createElement("div")
-            action.classList.add("action")
-
-            const img = document.createElement("img")
-            img.src = src
-
-            const label = document.createElement("span")
-            label.innerText = l
-
-            action.append(img)
-            action.append(label)
-
-            left.append(action)
-
-            if (auth.currentUser.uid != event.creator) func(action, label)
-
-        }
-
-        addAction(`${attending} Attending`, "../img/icons/profile.png", (button, span) => {
-            if (selfAttend) {
-                button.style.border = "3px solid var(--accent)"
-            }
-    
-            button.onclick = async function() {
-                if (selfAttend) {
-                    selfAttend = false
-                    button.style.border = "3px solid transparent"
-                    attending -= 1
-    
-                }
-                else {
-                    selfAttend = true
-                    button.style.border = "3px solid var(--accent)"
-                    attending += 1
-    
-                }
-                
-                span.innerText = `${attending} Attending`;
-                await setDoc(doc(db, "posts", id, "uData", auth.currentUser.uid), {
-                    attending: selfAttend
-                })
-                
-            }
-    
             
-        })
-
-
-        const open = document.createElement("div")
-        open.classList.add("action")
-
-        const openImage = document.createElement("img")
-
-        openImage.src = "../img/icons/arrow.png"
-
-        open.append(openImage)
         
-        open.onclick = function() {
-            window.location.href = "../event/index.html?e=" + id
+    </div>
+    `
+    content.appendChild(ev)
+
+    // actions
+
+    let attending = 0
+    
+    let selfAttend = false
+
+    const uData = await getEventUData(id)
+
+    uData.forEach((doc) => {
+        const data = doc.data()
+
+        if (data.attending) {
+            attending += 1
+        }
+        if (doc.id == auth.currentUser.uid && data.attending) selfAttend = true;
+    })
+
+    const left = document.createElement("div")
+    left.classList.add("row")
+
+    const actions = ev.querySelector(`.actions`)
+
+    actions.append(left)
+
+    function addAction(l, src, func) {
+        const action = document.createElement("div")
+        action.classList.add("action")
+
+        const img = document.createElement("img")
+        img.src = src
+
+        const label = document.createElement("span")
+        label.innerText = l
+
+        action.append(img)
+        action.append(label)
+
+        left.append(action)
+
+        if (auth.currentUser.uid != event.creator) func(action, label)
+
+    }
+
+    addAction(`${attending} Attending`, "../img/icons/profile.png", (button, span) => {
+        if (selfAttend) {
+            button.style.border = "3px solid var(--accent)"
         }
 
-        actions.append(open)
+        button.onclick = async function() {
+            if (selfAttend) {
+                selfAttend = false
+                button.style.border = "3px solid transparent"
+                attending -= 1
+
+            }
+            else {
+                selfAttend = true
+                button.style.border = "3px solid var(--accent)"
+                attending += 1
+
+            }
+            
+            span.innerText = `${attending} Attending`;
+            await setDoc(doc(db, "posts", id, "uData", auth.currentUser.uid), {
+                attending: selfAttend
+            })
+            
+        }
 
         
+    })
+
+
+    const open = document.createElement("div")
+    open.classList.add("action")
+
+    const openImage = document.createElement("img")
+
+    openImage.src = "../img/icons/arrow.png"
+
+    open.append(openImage)
+    
+    open.onclick = function() {
+        window.location.href = "../event/index.html?e=" + id
     }
+
+    actions.append(open)
+
+        
+    
+}
+
+export async function getEventUData(eventId) {
+    return await getDocs(query(collection(db, "posts", eventId, "uData")))
 }
 
 export async function createEvent(uid, cate, desc, date, location, cost, tags, title, agenda) {
@@ -268,6 +257,40 @@ export async function createEvent(uid, cate, desc, date, location, cost, tags, t
     return event.id;
 }
 
+export async function getEvent(eventId) {
+    let e = await getDoc(doc(db, "posts", eventId))
+
+    if (!e.exists()) {
+        console.error("Could not load event with id: " + eventId)
+        return {}
+    }
+
+    
+    let eventData = e.data()
+
+    return eventData
+}
+
+export async function getUserData(uid) {
+
+    let u = await getDoc(doc(db, "users", uid, "data", "public"))
+    if (!u.exists()) {
+        console.error("Could not load userData with creator id: " + uid)
+    };
+
+    let userData = u.data()
+    return userData
+}
+
+export async function getUsername(uid) {
+    let usernameRef = await getDoc(doc(db, "usernames", uid))
+
+    if (!usernameRef.exists()) {
+        console.error("Could not load username from uid: " + uid)
+    }
+
+    return usernameRef.data().username
+}
 
 export function toTitleCase(str) {
     return str.replace(/\w\S*/g, function(txt){
