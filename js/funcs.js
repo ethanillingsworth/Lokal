06d81757bbc5e7c4fd0a6fd7d930f84ede28b984
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2025-02-18 21:40:58 -0600

    add testing placeholders for weeks

--- js/funcs.js
+++ js/funcs.js
@@ -1247,7 +1247,7 @@ export class Calendar {
             weeks.append($("<h4/>").addClass("week").text(w))
         });
 
-        // calandar.append(weeks)
+        calandar.append(weeks)
 
         const days = $("<div/>")
             .addClass("days grid")
@@ -1271,18 +1271,18 @@ export class Calendar {
             const monthsDays = s.data[s.currentYear][s.currentMonth]
 
             Object.keys(monthsDays).forEach((d) => {
-                // if (d == 1) {
-                //     const off = new Date(`${s.currentYear}-${s.currentMonth}-${d}`).getDay()
-                //     offset(new Date(`${s.currentYear}-${s.currentMonth}-${d}`).getDay())
+                if (d == 1) {
+                    const off = new Date(`${s.currentYear}-${s.currentMonth}-${d}`).getDay()
+                    offset(new Date(`${s.currentYear}-${s.currentMonth}-${d}`).getDay())
 
-                //     if (off > 4) {
-                //         days.css("grid-template-rows", "repeat(6, 75px)")
-                //     }
-                //     else {
-                //         days.css("grid-template-rows", "repeat(5, 75px)")
+                    if (off > 4) {
+                        days.css("grid-template-rows", "repeat(6, 75px)")
+                    }
+                    else {
+                        days.css("grid-template-rows", "repeat(5, 75px)")
 
-                //     }
-                // }
+                    }
+                }
                 const dayData = monthsDays[d]
 
                 const day = $("<div/>")
